1947b7a0531b9df032e75e3132df85ca3d24a6a9
pts2td <pts2td@virginia.edu>
2014-06-22 20:45:25 -0400

    messing with permissions

--- app.rb
+++ app.rb
@@ -74,12 +74,12 @@ get "/" do
 
   if access_token
     @user    = @graph.get_object("me")
-    @friends = @graph.get_connections('me', 'friends')
-    @photos  = @graph.get_connections('me', 'photos')
-    @likes   = @graph.get_connections('me', 'likes').first(4)
+    #@friends = @graph.get_connections('me', 'friends')
+    #@photos  = @graph.get_connections('me', 'photos')
+    #@likes   = @graph.get_connections('me', 'likes').first(4)
 
     # for other data you can always run fql
-    @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
+    #@friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
   end
   erb :index
 end
